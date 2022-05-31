cd1777975799b0049fccd3076038cc0b43c5d40d
maxime.koster <maxime.koster@staffme.fr>
2022-05-31 23:30:14 +0200

    Better terrain generation

--- GODOT/Terrain.gd
+++ GODOT/Terrain.gd
@@ -3,6 +3,8 @@ extends Node2D
 var height = 40
 var width = 100
 
+var noise = OpenSimplexNoise.new()
+
 func fill_ground():
 	var ground = get_node("Ground")
 	var grass_tile = ground.tile_set.find_tile_by_name("Grass")
@@ -12,24 +14,28 @@ func fill_ground():
 	ground.update_bitmask_region()
 
 
-func fill_water():
-	var noise = OpenSimplexNoise.new()
-	noise.seed = randi()
-	noise.octaves = 4
-	noise.period = 30
-	noise.persistence = 0.8
-	
+func fill_water():	
 	var water = get_node("Water")
 	var water_tile = water.tile_set.find_tile_by_name("Water")
 	for x in width:
 		for y in height:
 			var noise_value = noise.get_noise_2d(x, y)
-			if (noise_value > 0):
+			print(noise_value)
+			if (noise_value > 0.3):
 				water.set_cell(x, y, water_tile)
 	water.update_bitmask_region()
 
 func _ready():
 	randomize()
+	noise.seed = randi()
+	noise.octaves = 1
+	noise.period = 10
+
+	# Debug + Extra options TODO Clean
+	# noise.persistance = 2
+	# noise.lacunarity = 50
+	# noise.get_image(100, 40).save_png("res://saved_texture.png")
+
 	fill_ground()
 	fill_water()
 
