ec85909a4b62caacc97713e8c7f7a51b1133070d
maxime.koster <maxime.koster@staffme.fr>
2022-06-01 17:25:51 +0200

    Removed logging

--- GODOT/Entities/Animals/Animal.gd
+++ GODOT/Entities/Animals/Animal.gd
@@ -1,7 +1,7 @@
 class_name Animal
 extends KinematicBody2D
 
-export var hunger_rate = 10 # Nb of seconds before death
+export var hunger_rate = 0.5 # Nb of seconds before death
 export var is_eating = false
 export var is_dying = false
 export var health = 100
@@ -13,11 +13,11 @@ func _ready():
 	pass # Replace with function body.
 
 func process_hunger(delta):
-	self.hunger -= delta * self.hunger_rate
-	if (self.hunger <= 0):
-		self.is_dying = true
+	if (!self.is_dying):
+		self.hunger -= delta * self.hunger_rate
+		if (self.hunger <= 0):
+			self.is_dying = true
 
 func _process(delta):
 	process_hunger(delta)
-	print(self.hunger)
 
