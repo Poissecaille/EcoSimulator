ef5d92710850330ced44a9e074143a7c9db8c3fc
Rayane Serir <serir_r@etna-alternance.net>
2022-06-02 17:09:28 +0200

    prey IA

--- GODOT/spawns/Sheep.gd
+++ GODOT/spawns/Sheep.gd
@@ -1,51 +1,118 @@
 extends KinematicBody2D
 
-export var speed = 100
-var health = 10
+enum BEHAVIOR {NORMAL, PANIC, WEAK}
+export(BEHAVIOR) var Behavior = BEHAVIOR.NORMAL
+
+export var speed = 50
+export var MAX_HEALTH = 10
+export var MAX_HUNGER = 10
+
+var hunger
+var health
 var screen_size
 var terrain
-export var is_eating = false
-export var is_dying = false
+var currentVelocity = Vector2.ZERO
+var resting = false
+var rng = RandomNumberGenerator.new()
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rng.randomize()
 	terrain=get_parent().get_node("Terrain")
 	screen_size = Vector2(terrain.width*32,terrain.height*32)
-
+	health=MAX_HEALTH
+	hunger=MAX_HUNGER
+	
+	
 func start(pos):
 	position = pos
 	show()
 	
 	
 func _process(delta):
-	process_inputs(delta)
-
-
-
-
-func process_inputs(delta):
 	$AnimatedSprite.play()
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+	check_health()
+	process_inputs()
+	move(delta)
+	update_animation()
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		# $HyenaAnimation.play()
-	
-	move_and_collide(velocity*delta)
-	if velocity.y > 0:
+
+func update_animation():
+	if currentVelocity.y > 0:
 		$AnimatedSprite.animation = "walk_bot"
-	if velocity.y < 0:
+	if currentVelocity.y < 0:
 		$AnimatedSprite.animation= "walk_top"
-	if velocity.x < 0 && velocity.y ==0: 
+	if currentVelocity.x < 0 && currentVelocity.y ==0: 
 		$AnimatedSprite.animation= "walk_left"
-	if velocity.x > 0 && velocity.y ==0: 
+	if currentVelocity.x > 0 && currentVelocity.y ==0: 
 		$AnimatedSprite.animation= "walk_right"
+	
+	
+func check_health():
+	if (health< MAX_HEALTH/2):
+		Behavior= BEHAVIOR.WEAK
+		
 
+func take_damage(damage):
+	health-= damage
+	$PanicTimer.start()
+	$MovementTimer.set_wait_time(0.2)
+	Behavior= BEHAVIOR.PANIC
+	
+	
+
+
+
+func move(delta):
+	if currentVelocity.length() > 0:
+		match Behavior:
+			BEHAVIOR.WEAK:
+				currentVelocity = currentVelocity.normalized() * (speed/2)
+			BEHAVIOR.NORMAL:
+				currentVelocity = currentVelocity.normalized() * speed
+			BEHAVIOR.PANIC:
+				currentVelocity = currentVelocity.normalized() * (speed*4)
+		move_and_collide(currentVelocity*delta)
+	
+	
+func process_inputs():
+	currentVelocity= Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		currentVelocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		currentVelocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		currentVelocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		currentVelocity.y -= 1
+
+	
+
+
+func _on_RegenTimer_timeout():
+	if(health< MAX_HEALTH and hunger > MAX_HUNGER/2):
+		health+=1
+		hunger-=1
+	
+
+
+func _on_MovementTimer_timeout():
+	if(resting):
+		currentVelocity = Vector2.ZERO
+	else :
+		currentVelocity.x=rng.randi_range(-1,1)
+		currentVelocity.y=rng.randi_range(-1,1)
+	resting = not resting
+	
+
+
+
+func _on_PanicTimer_timeout():
+	Behavior = BEHAVIOR.NORMAL
+	$MovementTimer.set_wait_time(2)
+
+
+func _on_TestActionTimer_timeout():
+	pass
